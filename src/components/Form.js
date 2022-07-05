4b9ce55fa4cbe499f5f85b193290d9c6a59b11da
yusufgozukara <yusufgozukara11@gmail.com>
2022-07-05 09:58:28 +0300

    form_v1

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,26 +7,33 @@ const Form = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const handleUsername = (e) => {
+        setUsername(e.target.value)
+        // console.log(e.target.value);
+    }
+
   return (
       <div>
       <h1  style={{marginTop:'5rem'}}>Siz de çekilişe katılmak ister misiniz?</h1>
   <form>
     <div className="mb-3">
     <label htmlFor="exampleInputEmail1" className="form-label">
-        Username
+        Username : (<span style={{color:'red'}}>{username}</span>)
       </label>
       <input
         type="text"
         className="form-control"
-        id="exampleInputEmail1"
+        id="exampleInputtext"
+        onChange={handleUsername}
       /> <br/>
       <label htmlFor="exampleInputEmail1" className="form-label">
-        Email address
+        Email address : (<span style={{color:'green'}}>{email}</span>) 
       </label>
       <input  style={{margin:'2rem'}}
         type="email"
         className="form-control"
         id="exampleInputEmail1"
+        onChange={(e) => setEmail(e.target.value)}
       />
     </div>
     <div className="mb-3">
